6c7262b5aa5166dd11795f4eef5ad5a8d88613c7
hieu <phuchieuct93@gmail.com>
2018-10-13 16:20:08 +0700

    Start course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule }    from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as MD from '@angular/material';
 import { MaterialModule } from './material.module';
-import { ServersComponent } from './servers/servers.component';
-import ServerComponent from './server/server.component';
-
+import { Header } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent,
-    HeroSearchComponent,
-    ServersComponent,
-    ServerComponent
+    Header
     
   ],
   imports: [
